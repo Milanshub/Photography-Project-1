63d71d7d79fde0b3804786389a1bf6aad8110b13
Milan <m26030820@gmail.com>
2024-02-08 20:45:24 +0100

    Navbar react-bootstrap

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,42 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import "./header.css"; 
+
+function Header(){
+    return (
+    
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container className='navbar-header'>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#main">Main</Nav.Link>
+                <NavDropdown title="Gallery" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Services</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Portrait
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Commercial</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Retouching
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#services">Services</Nav.Link>
+                <Nav.Link href="#testimonials">Testimonials</Nav.Link>
+                <Nav.Link href="#contacts">Contacts</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+      );
+   
+    
+
+}
+
+export default Header; 
