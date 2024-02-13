29d0c91d1cfe8bc1a1f5eb616052b87d1b45fc17
Milan <m26030820@gmail.com>
2024-02-13 09:46:20 +0100

    aligning items

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -2,13 +2,38 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+
+function handelMoreClick (){
+  alert("Good Morning babe")
+}
+
 function Gallery(){
     return (
-        <Container>
+        <Container className='gallery-container' >
           <Row className="gallery-row">
-            <Col className='col-1'>Portrait</Col>
-            <Col className='col-1'>Campaign</Col>
-            <Col className='col-1'>Retouching</Col>
+            <Col className='col-1'>  
+            <div className='gallery-div'>
+             <h2  className='gallery-h2 portrait'>Portrait</h2>
+              <button className='gallery-btn' 
+              onClick={handelMoreClick}>more
+              </button> 
+              </div>
+            </Col>
+            <Col className='col-1'>
+              <di>
+              <h2  className='gallery-h2 campaign'>Campaign</h2>
+              <button className='gallery-btn' 
+               onClick={handelMoreClick}>more
+              </button>
+             </di>
+            </Col>    
+            <Col className='col-1'>
+              <div> 
+               <h2 className='gallery-h2 retouching'>Retouching</h2>
+               <button className='gallery-btn' 
+                onClick={handelMoreClick}>more</button>
+              </div>
+            </Col>        
           </Row>
         </Container>
       );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header(){
     return (
     <Navbar expand="lg" className="navbar-header">
           <Container className='navbar-header-inner'>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">Likakambur Gallery</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
